072b7bdf1ca33989e8fdf51cb43f36527e2bd444
Wagner Terry <goncalveswagner15@gmail.com>
2024-04-07 17:36:04 -0300

    feat: criando controllers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return "Ola mundo";
+// });
+
+Route::get('/', 'PrincipalController@principal');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+
+Route::get('/contato', 'ContatoController@contato');
